b80a7c85ea73e8e197ac4b20b72bb2fcc7b8e9e2
flaviobuchmann <flavio.buchmann@bluewin.ch>
2024-04-15 15:41:33 +0200

    Created Repository

---
+++ Laracast_Practice/notes.php
@@ -0,0 +1,3 @@
+<?php
+//Start the PHP Server in Laragon Terminal
+//php -S localhost:8888 -t public
---
+++ Laracast_Practice/views/403.php
@@ -0,0 +1,13 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold">403 You are not authorized to view this Page!</h1>
+        <p class="mt-4">
+            <a href="/" class="text-blue underline">Go back Home</a>
+        </p>
+    </div>
+</main>
+
+<?php require('partials/footer.php') ?>
